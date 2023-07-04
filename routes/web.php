ae568c24098b035c384e3df98a98a92757fc0f4b
Jodzav2 <105559220+Jodzirv2@users.noreply.github.com>
2023-07-04 18:47:15 +0200

    Make adminlte functional

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/activate/{token}', [AuthController::class, 'activateAccount'])->name('activate');
+Route::get('/admin',[AdminController::class,'index'])->name('admin-home');
