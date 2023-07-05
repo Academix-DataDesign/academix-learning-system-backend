04ef1901b6b54830947af124eea668bdbc49e8f2
Nikola <perisicnikola37@gmail.com>
2023-07-05 17:30:08 +0200

    Remove admin lte

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/activate/{token}', [AuthController::class, 'activateAccount'])->name('activate');
-Route::get('/admin',[AdminController::class,'index'])->name('admin-home');
+// Route::get('/admin', [AdminController::class, 'index'])->name('admin-home');
