0ae3c71cdf2b692e343ef4992a5cad240016fa74
Nikola <perisicnikola37@gmail.com>
2023-07-29 15:01:18 +0200

    Finish integration with email and slack notifications

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('01:30');
     }
 
     /**
@@ -20,7 +22,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
